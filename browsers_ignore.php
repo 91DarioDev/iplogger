55d82adbeea049bf6a6a76272a2836dc44740885
MattyDev2314 <63481743+MattyDev2314@users.noreply.github.com>
2021-02-02 03:24:32 +0100

    Update browsers_ignore.php
    New browsers added in the array

--- browsers_ignore.php
+++ browsers_ignore.php
@@ -2,6 +2,10 @@
 $BROWSERS_TO_IGNORE = array(
 	'TelegramBot(like TwitterBot)',
     'TelegramBot (like TwitterBot)',
-    'bitlybot/3.0 (+http://bit.ly/)'
+    'bitlybot/3.0 (+http://bit.ly/)',
+    'Mozilla/5.0 (compatible; Nimbostratus-Bot/v1.3.2; http://cloudsystemnetworks.com)',
+    'Mozilla/5.0 (compatible; NetcraftSurveyAgent/1.0; +info@netcraft.com)',
+    'Mozilla/5.0 (Windows NT 6.0; rv:2.0) Gecko/20100101 Firefox/4.0 Opera 12.14',
+    'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4325)'
 );
 ?>
