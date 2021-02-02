80de8814f718eb36f8d713611e6dffe3a621f638
Mattia <63481743+MattyDev2314@users.noreply.github.com>
2021-02-02 16:37:36 +0100

    Update browsers_ignore.php

--- browsers_ignore.php
+++ browsers_ignore.php
@@ -8,6 +8,8 @@ $BROWSERS_TO_IGNORE = array(
     'Mozilla/5.0 (Windows NT 6.0; rv:2.0) Gecko/20100101 Firefox/4.0 Opera 12.14',
     'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4325)',
     'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)',
-    'Mozilla/5.0 zgrab/0.x'
+    'Mozilla/5.0 zgrab/0.x',
+    'Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)',
+    'aria2/1.35.0'
 );
 ?>
