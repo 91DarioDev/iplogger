3d9ea2214a443b0b3a5922bdf47f14e8c91d998d
Mattia <63481743+MattyDev2314@users.noreply.github.com>
2021-02-02 13:47:27 +0100

    Update browsers_ignore.php

--- browsers_ignore.php
+++ browsers_ignore.php
@@ -5,6 +5,9 @@ $BROWSERS_TO_IGNORE = array(
     'bitlybot/3.0 (+http://bit.ly/)',
     'Mozilla/5.0 (compatible; Nimbostratus-Bot/v1.3.2; http://cloudsystemnetworks.com)',
     'Mozilla/5.0 (compatible; NetcraftSurveyAgent/1.0; +info@netcraft.com)',
+    'Mozilla/5.0 (Windows NT 6.0; rv:2.0) Gecko/20100101 Firefox/4.0 Opera 12.14',
+    'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4325)',
+    'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)',
     'Mozilla/5.0 zgrab/0.x'
 );
 ?>
